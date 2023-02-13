3f14a187c363ff36be3d037407cb8945f2b59a81
AliSenina <122289718+AliSenina@users.noreply.github.com>
2023-02-13 12:36:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+// Завдання 1
+// let ask = +prompt("Введіть пароль");
+// let password = 123;
+// let i = 4;
+// while(i>0){
+
+//     if(ask===password){
+//         alert(`Запрошуємо на сайт!`)
+
+//     }
+//     else if(ask!==password ){
+//         ask = +prompt(`Залишилось ${i} спроби!`)
+//     }
+//     i--
+//     while(i===0){
+
+//             alert(`Будь ласка спробуйте пізніше!`)}
+
+//     }
+
+// Завдання 2
+// let n=prompt('write n');
+// let result=1;
+// let i=1
+// while(i<=n){
+//     result*=i; i++
+
+// }
+// alert(result)
+
+//Завдання 2
+// let i = 1000;
+// while (i <= 9999) {
+//   i += 3;document.write(` ${i} `);
+// }
+
+//Завдання 4
+// let n = -1;
+// let i = 0;
+// while (i <= 55) {
+//   n += 2;
+//   i++;
+//   document.write(` ${n} `);
+// }
+//Завдання 5
+// let i = 95;
+// while (i >0) {
+//   i = i - 5;
+//   document.write(` ${i} `);
+// }
+
+// Завдання 6
+
+// let n = 1;
+// let i = 1;
+// while (i < 20) {
+//   n = 2 * n;
+//   i++;
+//   document.write(` ${n} `);
+// }
+
+// Завдання 7
+// let a = 2;
+
+// while (a<10000) {
+//   a = 2 * a - 1;
+//   document.write(` ${a} `);
+
+// }
+
+// Завдання 8
+// let a=-166;
+// while( a<100){
+//     a=2*a+200;
+//     if(a>-9|| a>-100|| a>9){
+//         document.write (` ${a} `)  }
+//     }
+
+// Завдання 9
+
+// let n1 = +prompt("Введіть число");
+
+// let n2 = +prompt("Введіть степень");
+
+// let result = 1;
+// let check;
+// let i = 1;
+// while (i <= n2) {
+//   result *= n1;
+//   i++;
+
+//   if (n2 < 0) {
+//     check = -n2;
+//     n2 = check;
+//   }
+// }
+// alert(`Result = ${result}`);
